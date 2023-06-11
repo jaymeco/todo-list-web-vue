83010a49a290db3db7c54bf1f3e0418b6a332854
Jayme Anunciação <jaymeanunciaca.hrzon@gmail.com>
2023-06-11 21:08:42 +0000

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
+import { createApp } from 'vue';
+// Vuetify
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(vuetify)
+  .mount('#app');
