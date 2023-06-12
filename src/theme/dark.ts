e84d0db2d59b54de20fda20125d809c8d98a2acd
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-06-12 02:05:13 +0000

    feature(theme/dark): Cria configuração do tema dark.

---
+++ src/theme/dark.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ThemeDefinition } from 'vuetify';
+
+const darkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: '#161616',
+    primary: '#FF1616',
+    'primary-lighter': '#FF5A5A',
+  },
+};
+
+export default darkTheme;
